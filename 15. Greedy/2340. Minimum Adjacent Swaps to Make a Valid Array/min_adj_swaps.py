2dfe47755370bb50b797d2a5f6c90c97917936e2
Sudharshan Chakravarthy <s3chakra@ucsd.edu>
2024-07-17 22:56:42 -0700

    min adj swaps - comments and edge case explanation

--- 15. Greedy/2340. Minimum Adjacent Swaps to Make a Valid Array/min_adj_swaps.py
+++ 15. Greedy/2340. Minimum Adjacent Swaps to Make a Valid Array/min_adj_swaps.py
@@ -8,10 +8,14 @@ class Solution:
             if num < smallest:
                 smallest_idx = k
                 smallest = num
-            if num >= largest:
+            if num >= largest: # >= because we want the right most largest value
                 largest_idx = k
                 largest = num
 
-        res = (last_index - largest_idx) + smallest_idx
+        # the result is simply the number of places required by the right most largest index to reach the end 
+        # plus the left most smallest number to reach index 0
+        res = (last_index - largest_idx) + (smallest_idx - 0)
 
-        return res - 1 if smallest_idx > largest_idx else res      
+        # res-1 is needed beacuse when the small_idx is greater than the large_idx
+        # there is excatly one swap when they meet (which is overlapping) so we subtract by 1.
+        return res - 1 if smallest_idx > largest_idx else res     
