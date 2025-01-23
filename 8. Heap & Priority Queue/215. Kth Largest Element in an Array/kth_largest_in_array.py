570037cda76081465dde3be42d612a3e2ebead12
Sudharshan Chakravarthy <s3chakra@ucsd.edu>
2025-01-23 00:50:59 -0800

    215 kth largest optimized

--- 8. Heap & Priority Queue/215. Kth Largest Element in an Array/kth_largest_in_array.py
+++ 8. Heap & Priority Queue/215. Kth Largest Element in an Array/kth_largest_in_array.py
@@ -1,6 +1,19 @@
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        # O(NlogK) time, O(k) space
+        min_heap = []
+
+        for num in nums:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, num)
+            else:
+                heapq.heappushpop(min_heap, num)
+        
+        return min_heap[0]
+
+        # O(NlogN) time, O(N) space
         nums = [-i for i in nums]
         heapq.heapify(nums)
 
